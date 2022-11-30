d0acd9712452e73d2baf28fcc635d2a33700d0ac
guzuv <110692178+guzuv@users.noreply.github.com>
2022-11-30 15:39:45 -0500

    Commit from TeleportHQ

--- src/views/e-m-a-i-l.js
+++ src/views/e-m-a-i-l.js
@@ -11,6 +11,20 @@ const EMAIL = (props) => {
         <title>EMAIL - GAMES!!!!!!!!!!!!!!!</title>
         <meta property="og:title" content="EMAIL - GAMES!!!!!!!!!!!!!!!" />
       </Helmet>
+      <h1 className="e-m-a-i-l-text">
+        <span>
+             
+          <span
+            dangerouslySetInnerHTML={{
+              __html: ' ',
+            }}
+          />
+        </span>
+        <span className="e-m-a-i-l-text2">     The email is </span>
+        <br className="e-m-a-i-l-text3"></br>
+        <span className="e-m-a-i-l-text4">helpline@gamesplus.ml</span>
+        <br></br>
+      </h1>
     </div>
   )
 }
--- src/views/home.js
+++ src/views/home.js
@@ -280,11 +280,10 @@ const Home = (props) => {
                 <path d="M1088 901.166c0 45.5 26.028 84.908 64 104.184v15.938c-10.626 1.454-21.472 2.224-32.5 2.224-68.008 0-129.348-28.528-172.722-74.264-26.222 6.982-54.002 10.752-82.778 10.752-159.058 0-288-114.616-288-256s128.942-256 288-256c159.058 0 288 114.616 288 256 0 55.348-19.764 106.592-53.356 148.466-6.824 14.824-10.644 31.312-10.644 48.7zM512 0c278.458 0 504.992 180.614 511.836 405.52-49.182-21.92-103.586-33.52-159.836-33.52-95.56 0-185.816 33.446-254.138 94.178-70.846 62.972-109.862 147.434-109.862 237.822 0 44.672 9.544 87.888 27.736 127.788-5.228 0.126-10.468 0.212-15.736 0.212-27.156 0-53.81-1.734-79.824-5.044-109.978 109.978-241.25 129.7-368.176 132.596v-26.916c68.536-33.578 128-94.74 128-164.636 0-9.754-0.758-19.33-2.164-28.696-115.796-76.264-189.836-192.754-189.836-323.304 0-229.75 229.23-416 512-416z"></path>
               </svg>
               <h1 className="home-text21">
-                <span className="HeadingOne">Become a Member</span>
+                <span className="HeadingOne">Email us</span>
                 <br></br>
               </h1>
-              <span className="home-text24">FOR FREE</span>
-              <Link to="/job-mod" className="home-navlink7">
+              <Link to="/e-m-a-i-l" className="home-navlink7">
                 <OutlineBlackButton
                   GetGaming="get started"
                   className="home-component09"
@@ -297,33 +296,33 @@ const Home = (props) => {
       <footer className="home-footer">
         <div className="home-container39">
           <div className="home-container40">
-            <span className="home-text25">SOFT</span>
+            <span className="home-text24">SOFT</span>
             <span>Copyright © 2022  by Gamesplus.</span>
           </div>
           <div className="home-container41">
             <div className="home-container42">
-              <span className="home-text27 Large">Company</span>
+              <span className="home-text26 Large">Company</span>
               <a href="#About" className="home-link Large">
                 About Us
               </a>
               <Link to="/job-mod" className="home-navlink8 Large">
                 Careers
               </Link>
-              <Link to="/" className="home-text28 Large">
+              <Link to="/" className="home-text27 Large">
                 Press
               </Link>
             </div>
             <div className="home-container43">
-              <span className="home-text29 Large">Pages</span>
-              <span className="home-text30 Large">Login</span>
-              <span className="home-text31 Large">Register</span>
-              <span className="home-text32 Large">About</span>
+              <span className="home-text28 Large">Pages</span>
+              <span className="home-text29 Large">Login</span>
+              <span className="home-text30 Large">Register</span>
+              <span className="home-text31 Large">About</span>
             </div>
             <div className="home-container44">
-              <span className="home-text33 Large">Products</span>
-              <span className="home-text34 Large">Free</span>
-              <span className="home-text35 Large">PRO</span>
-              <span className="home-text36 Large">Latest</span>
+              <span className="home-text32 Large">Products</span>
+              <span className="home-text33 Large">Free</span>
+              <span className="home-text34 Large">PRO</span>
+              <span className="home-text35 Large">Latest</span>
             </div>
           </div>
         </div>
