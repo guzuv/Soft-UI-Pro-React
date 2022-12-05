17986f090e2c6bb12cb2ed1c0271c3e5faeb6ef0
guzuv <110692178+guzuv@users.noreply.github.com>
2022-12-05 13:53:43 -0500

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './style.css'
 import Games2 from './views/games2'
 import ReadMore from './views/read-more'
+import MoreGames from './views/more-games'
 import JobMod from './views/job-mod'
 import EMAIL from './views/e-m-a-i-l'
 import ComingSoon from './views/coming-soon'
@@ -12,7 +13,6 @@ import Profile from './views/profile'
 import Games from './views/games'
 import Page from './views/page'
 import Home from './views/home'
-import MoreGames from './views/more-games'
 
 const App = () => {
   return (
@@ -20,6 +20,7 @@ const App = () => {
       <div>
         <Route component={Games2} exact path="/games2" />
         <Route component={ReadMore} exact path="/read-more" />
+        <Route component={MoreGames} exact path="/more-games" />
         <Route component={JobMod} exact path="/job-mod" />
         <Route component={EMAIL} exact path="/e-m-a-i-l" />
         <Route component={ComingSoon} exact path="/coming-soon" />
@@ -27,7 +28,6 @@ const App = () => {
         <Route component={Games} exact path="/games" />
         <Route component={Page} exact path="/page" />
         <Route component={Home} exact path="/" />
-        <Route component={MoreGames} exact path="/more-games" />
       </div>
     </Router>
   )
